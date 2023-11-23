01a0b94c204f2884f23011bf402a5170cccc6feb
Lars Eggert <lars@eggert.org>
2023-11-23 09:23:31 +0200

    cache

--- neqo-crypto/build.rs
+++ neqo-crypto/build.rs
@@ -160,7 +160,6 @@ fn build_nss(dir: PathBuf) {
     if target.strip_prefix("aarch64-").is_some() {
         build_nss.push(String::from("--target=arm64"));
     }
-    println!("bash {}", get_bash().display());
     let status = Command::new(get_bash())
         .args(build_nss)
         .current_dir(dir)
