12dafeaec5ba0187d89eff6e0b40745cf5f4b0d5
Lars Eggert <lars@eggert.org>
2023-11-30 16:35:37 +0200

    git again

--- neqo-crypto/build.rs
+++ neqo-crypto/build.rs
@@ -138,12 +138,12 @@ fn get_bash() -> PathBuf {
             if env::consts::OS == "windows" {
                 // On Windows w/o MOZILLABUILD, we need to invoke a different bash.exe
                 // C:\Program Files\Git\bin\bash.EXE
-                PathBuf::from("C:\\").join("usr").join("bin").join("bash.exe")
-                // PathBuf::from("C:\\")
-                //     .join("Program Files")
-                //     .join("Git")
-                //     .join("bin")
-                //     .join("bash.EXE")
+                // PathBuf::from("C:\\").join("usr").join("bin").join("bash.exe")
+                PathBuf::from("C:\\")
+                    .join("Program Files")
+                    .join("Git")
+                    .join("bin")
+                    .join("bash.EXE")
             } else {
                 PathBuf::from("bash")
             }
